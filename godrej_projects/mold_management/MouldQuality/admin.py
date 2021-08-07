1baf5e5507a1f79b31e55f9b7162a4629550edfa
MkshSahani <msco18333@gmail.com>
2021-08-07 18:55:12 +0530

    quality analysis updated

--- godrej_projects/mold_management/MouldQuality/admin.py
+++ godrej_projects/mold_management/MouldQuality/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import PPMData 
 
-# Register your models here.
+admin.site.register(PPMData) # PPM Data. 
--- godrej_projects/mold_management/MouldQuality/models.py
+++ godrej_projects/mold_management/MouldQuality/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# ------------------------------------- 
+class PPMData(models.Model): 
+
+    new_code =  models.CharField(max_length=100)
+    vendor_name = models.CharField(max_length=200)
+    ppm_data_added = models.DateField(auto_now_add=True)
+    total_number_of_lot = models.IntegerField()
+    total_number_of_lot_rejected = models.IntegerField()
+
+
+    def ppm(self): 
+        
+        number_of_lot_accepted = self.total_number_of_lot - self.total_number_of_lot_rejected
+        return number_of_lot_accepted / (10 ** 6)
+
+    
+    def __str__(self): 
+        return self.new_code + self.vendor_name
+
+    
